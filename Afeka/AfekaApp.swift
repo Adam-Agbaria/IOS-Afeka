6ba89d2eaa772890fd0db850ecde3a179856a61c
Adam-Agbaria <adambusiness105@gmail.com>
2025-05-31 15:30:52 +0300

    Commit

---
+++ Afeka/AfekaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfekaApp.swift
+//  Afeka
+//
+//  Created by Adam Agbaria on 31/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct AfekaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AfekaTests/AfekaTests.swift
@@ -0,0 +1,17 @@
+//
+//  AfekaTests.swift
+//  AfekaTests
+//
+//  Created by Adam Agbaria on 31/05/2025.
+//
+
+import Testing
+@testable import Afeka
+
+struct AfekaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
